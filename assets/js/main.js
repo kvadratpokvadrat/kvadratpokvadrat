eb279bb4c48edba3dc4b39fecfec5d01eda84c24
kvadratpokvadrat <podcastkvadratpokvadrat@gmail.com>
2026-01-26 03:38:29 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -234,3 +234,27 @@ function setCounter(id, val) {
   loadEpisodes();
 
 })();
+document.addEventListener("DOMContentLoaded", () => {
+  const hero = document.querySelector(".hero");
+  const social = document.getElementById("socialFloat");
+
+  if (!hero || !social) return;
+
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        // hero je vidljiv → sakrij
+        social.classList.remove("visible");
+      } else {
+        // hero nije vidljiv → prikaži
+        social.classList.add("visible");
+      }
+    },
+    {
+      threshold: 0.4
+    }
+  );
+
+  observer.observe(hero);
+});
+
